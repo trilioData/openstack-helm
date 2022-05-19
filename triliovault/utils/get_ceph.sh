075fbd05667e9e79a6f571c19ccfb544bce82cf8
shyam-biradar <shyam.biradar@trilio.io>
2022-05-19 17:38:44 +0530

    Updated default ceph user to nova

--- triliovault/utils/get_ceph.sh
+++ triliovault/utils/get_ceph.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-CINDER_CEPH_KEYRING=$(kubectl -n rook-ceph get secrets/rook-ceph-client-cinder --template={{.data.cinder}} | base64 -d)
+NOVA_CEPH_KEYRING=$(kubectl -n rook-ceph get secrets/rook-ceph-client-nova --template={{.data.nova}} | base64 -d)
 kubectl -n openstack get configmap/rook-ceph-config -o "jsonpath={.data['ceph\.conf']}" > ../templates/bin/_triliovault-ceph.conf.tpl
 
 cd ../
@@ -12,7 +12,7 @@ tee > values_overrides/ceph.yaml  << EOF
 ceph:
   enabled: true
   conf: ""
-  rbd_user: cinder
-  keyring: $CINDER_CEPH_KEYRING
+  rbd_user: nova
+  keyring: $NOVA_CEPH_KEYRING
 EOF
 
