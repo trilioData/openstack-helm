f01160b0b4aa913c1b09557df6da067ad9704d08
shyam-biradar <shyam.biradar@trilio.io>
2022-05-20 09:12:26 +0530

    Added wait for pods script

--- triliovault/utils/install_triliovault_chart_mosk.sh
+++ triliovault/utils/install_triliovault_chart_mosk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
-
 cd ../../
+
+
 helm upgrade --install triliovault ./triliovault --namespace=triliovault \
 --values=./triliovault/values_overrides/image_pull_secrets.yaml \
 --values=./triliovault/values_overrides/conf_triliovault.yaml \
@@ -8,3 +9,8 @@ helm upgrade --install triliovault ./triliovault --namespace=triliovault \
 --values=./triliovault/values_overrides/admin_creds.yaml \
 --values=./triliovault/values_overrides/tls_public_endpoint.yaml \
 --values=./triliovault/values_overrides/ceph.yaml
+
+echo -e "Waiting for triliovault pods to get into running state"
+./triliovault/utils/wait-for-pods.sh triliovault
+
+kubectl get pods
